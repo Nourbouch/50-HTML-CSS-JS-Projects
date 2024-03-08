a42ae70cac0f52e3cafb0ef7f5b99c4856981315
NourBouch <islambouchenak2001@gmail.com>
2024-03-08 13:39:29 +0100

    Done with HTML && JS FUNCTIONALITY

---
+++ Temperature_Converter/src/js/script.js
@@ -0,0 +1,51 @@
+const elements = {
+  temperatureValue: document.getElementById("temp_value"),
+  temperatureUnit: document.getElementById("temp_unit"),
+  temperatureConvertTo: document.getElementById("temp_convert_to"),
+  result: document.getElementById("result"),
+};
+
+elements.temperatureValue.addEventListener("input", () => {
+  const { value } = elements.temperatureValue;
+
+  if (value !== "") {
+    const temperatureValue = parseFloat(value);
+    const convertTo = elements.temperatureConvertTo.value;
+    const unit = elements.temperatureUnit.value;
+
+    let convertedValue;
+
+    switch (convertTo) {
+      case "celsius":
+        convertedValue =
+          unit === "celsius"
+            ? temperatureValue
+            : unit === "fahrenheit"
+            ? (temperatureValue - 32) / 1.8
+            : temperatureValue - 273.15;
+        break;
+      case "fahrenheit":
+        convertedValue =
+          unit === "fahrenheit"
+            ? temperatureValue
+            : unit === "celsius"
+            ? temperatureValue * 1.8 + 32
+            : (temperatureValue - 273.15) * 1.8 + 32;
+        break;
+      case "kelvin":
+        convertedValue =
+          unit === "kelvin"
+            ? temperatureValue
+            : unit === "celsius"
+            ? temperatureValue + 273.15
+            : (temperatureValue - 32) / 1.8 + 273.15;
+        break;
+      default:
+        break;
+    }
+
+    const resultUnit =
+      convertTo === "kelvin" ? "K" : `°${convertTo.toUpperCase()}`;
+    elements.result.innerHTML = `${convertedValue.toFixed(2)}${resultUnit}`;
+  }
+});
