b24c7a3571f97c95cb169700bcb2cfa71eef7d3b
NourBouch <islambouchenak2001@gmail.com>
2024-03-13 18:41:12 +0100

    JavaScript - Done

--- Number_Guessing_Game/src/js/script.js
+++ Number_Guessing_Game/src/js/script.js
@@ -1,1 +1,22 @@
+const input = document.getElementById("number")
+const btn = document.getElementById("btn")
+const result = document.getElementById("result")
 
+btn.addEventListener("click",()=>{
+  if(input.value !== '' && input.value.length > 0){
+     let val = parseInt(input.value);
+    const random = Math.floor(Math.random() * 3)
+    if(val !== isNaN()){
+        result.innerText =" no"
+  }
+    if(val === random ){
+        result.innerText = "You are correct"
+  }
+    else{
+        result.innerText = "The random number is  " + random + " YOU ARE WRONG!"
+    }
+  }
+  else{
+    alert('Please Enter a valid number !')
+  }
+})
