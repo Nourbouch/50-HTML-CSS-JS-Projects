292177da03dc44bd9b397c15d385ecceca9a8785
NourBouch <islambouchenak2001@gmail.com>
2024-03-20 21:51:10 +0100

    generate quote done

--- Random_Quote_Generator/src/js/script.js
+++ Random_Quote_Generator/src/js/script.js
@@ -1,1 +1,23 @@
+const btnCreate = document.getElementById("create");
+const btnsGenerate = document.getElementById("generate");
+const btnDownload = document.getElementById("download");
+const quoteContainer = document.querySelector(".quote-container");
+
+async function getRandomQuote() {
+  const apiUrl = "https://api.quotable.io/random";
+
+  try {
+    const response = await fetch(apiUrl);
+    const data = await response.json();
+    const quote = data.content;
+    quoteContainer.innerHTML = `<h2>${quote}</h2>`;
+  } catch (error) {
+    console.error('Error fetching quote:', error);
+  }
+}
+
+
+
+btnsGenerate.addEventListener("click", getRandomQuote);
+btnDownload.addEventListener("click", downloadImage);
 
